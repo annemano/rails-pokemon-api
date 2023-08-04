c9ddd2d8018dcdde172cdc5b11bdaa3b44499e5f
Anne-Marie Nault <anne.ma@gmail.com>
2023-08-04 18:51:23 -0400

    Move error handling to ApplicationController

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -9,48 +9,33 @@ class Api::V1::PokemonsController < ApplicationController
     @pokemons = Pokemon.order(:number, :name)
     @pagy, @pokemons = pagy(@pokemons) if params[:page]
 
-    if @pokemons
-      render json: {status: "Success", message: "Fetched Pokemons successfully", data: @pokemons}, status: :ok
-    else
-      render json: @friends.errors, status: :bad_request
-    end
+    render json: {status: "Success", message: "Fetched Pokemons successfully", data: @pokemons}, status: :ok
   end
 
   # GET api/v1/pokemons/:id
   def show
-    if @pokemon
-      render json: {status: "Success", message: "Fetched #{@pokemon.name} successfully", data: @pokemon}, status: :ok
-    else
-      render json: @pokemon.errors, status: :bad_request
-    end
+    render json: {status: "Success", message: "Fetched #{@pokemon.name} successfully", data: @pokemon}, status: :ok
   end
 
   # POST api/v1/pokemons/
   def create
-    @pokemon = Pokemon.new(pokemon_params)
-    if @pokemon.save
-      render json: {status: "Success", message: "#{@pokemon.name} was created successfully!", data: @pokemon}, status: :created
-    else
-      render json: @pokemon.errors, status: :unprocessable_entity
-    end
+    @pokemon = Pokemon.create!(pokemon_params)
+
+    render json: {status: "Success", message: "#{@pokemon.name} was created successfully!", data: @pokemon}, status: :created
   end
 
   # PUT api/v1/pokemons/:id
   def update
-    if @pokemon.update(pokemon_params)
-      render json: {status: "Success", message: "#{@pokemon.name} was updated successfully!", data: @pokemon}, status: :ok
-    else
-      render json: @pokemon.errors, status: :unprocessable_entity
-    end
+    @pokemon.update(pokemon_params)
+
+    render json: {status: "Success", message: "#{@pokemon.name} was updated successfully!", data: @pokemon}, status: :ok
   end
 
   # DELETE api/v1/pokemons/:id
   def destroy
-    if @pokemon.destroy
-      render json: {status: "Success", message: "#{@pokemon.name} was deleted successfully!"}, status: :ok
-    else
-      render json: @pokemon.errors, status: :unprocessable_entity
-    end
+    @pokemon.destroy
+
+    render json: {status: "Success", message: "#{@pokemon.name} was deleted successfully!"}, status: :ok
   end
 
   private
